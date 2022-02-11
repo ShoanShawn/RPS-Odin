b3b26129aa77e1c908609b7ac201c7afcb2f3360
shoanshawn <shoanshawn@gmail.com>
2022-02-11 12:07:11 -0800

    Added 5 round system

--- myscripts.js
+++ myscripts.js
@@ -1,37 +1,73 @@
-let playGame = confirm("RPS?");
-if(playGame) {
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-    if(playerSelection) {
-        let playerOne = playerSelection.trim().toLowerCase();
-        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors"){
-
-            let computerSelection = Math.floor(Math.random() *3 + 1);
-            let computer = computerSelection === 1 ? "rock"
-                : computerSelection === 2 ? "paper"
-                : "scissors";
-
-            let result = 
-            playerOne === computer
-                ? "Tie Game!"
-                : playerOne === "rock" && computer === "paper"
-                ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                : playerOne === "paper" && computer === "scissors"
-                ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                : playerOne === "scissors" && computer === "rock"
-                ? `playerOne: ${playerOne}\nComputer: ${computer} \nComputer wins!`
-                : `playerOne: ${playerOne}\nComputer: ${computer} \nplayerOne wins!`
-                alert(result);
-        } else {
-            alert("STOP WASTING MY TIME MAN!");
-        }
+function computerPlay() {
+    let random = Math.random();
+    if (random <= 0.3333) {
+      return "paper";
+    } else if (random >= 0.6666) {
+      return "rock";
     } else {
-        alert("STOP WASTING MY TIME MAN!");
+      return "scissors";
     }
-    
-} else {
-    alert("STOP WASTING MY TIME MAN!");
+  }
+  
+  
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+      if (computerSelection === "paper") {
+        computerScore++;
+        return lose;
+      } else if (computerSelection === "rock") {
+        return tie;
+      } else {
+        userScore++;
+        return win;
+      }
+    }
+  
+    if (playerSelection.toLowerCase() === "scissors") {
+      if (computerSelection === "paper") {
+        userScore++;
+        return win;
+      } else if (computerSelection === "rock") {
+        computerScore++;
+        return lose;
+      } else {
+        return tie;
+      }
+    }
+  
+    if (playerSelection.toLowerCase() === "paper") {
+      if (computerSelection === "paper") {
+        return tie;
+      } else if (computerSelection === "rock") {
+        userScore++;
+        return win;
+      } else {
+        computerScore++;
+        return lose;
+      }
+    }
+  }
+  
+  
+  let userScore = parseInt(0);
+  let computerScore = parseInt(0);
+  let win = "You win"
+  let lose = "You lose"
+  let tie = "It is a tie"
+
+  for(var i=0; i<5; i++){
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    const computerSelection = computerPlay()
+    console.log(playRound(playerSelection, computerSelection))
+    console.log("your score = " + userScore);
+    console.log("Computer's score = " + computerScore);
+  }
+
+if(computerScore > userScore){
+    console.log("Computer Wins")
+} else if (userScore > computerScore){
+    console.log("You win")
+} else if (computerScore = userScore) {
+    console.log("Tie game")
 }
-
-
-
 
